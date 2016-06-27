550cbb0d36b6dffc75953e48d49177c0341a4d34
Aeomi <aeomei@gmail.com>
2016-06-27 06:59:31 -0400

    Support for client parsing aos version, serverside logging w/ color

--- src/server.ts
+++ src/server.ts
@@ -2,38 +2,73 @@ import * as express     from "express";
 import * as http        from "http";
 import * as bodyParser  from "body-parser";
 import * as SocketIO    from "socket.io";
+import * as fileSystem  from "fs";
+
+//TODO: Figure out how to import this via ES2015
+let tc = require("colors/safe");
 
 
-// Configuration
+/* -----------------------------------------
+|	Express server configuration and setup
+| ----------------------------------------- */
+
 const PORT_NUMBER:number = process.env.PORT || 8080;
 const DEBUG_ENABLED:boolean = true;
 
-const app = express();
-const server:http.Server = http.createServer(app);
-const io = SocketIO.listen(server);
+const PACKAGE_JSON:any = JSON.parse(fileSystem.readFileSync("../package.json", 'utf8'));
+const AOS_VERSION = PACKAGE_JSON.version; //TODO: Send the version to the client
+	//have the client display it in the page title
+const AOS_BUILD = PACKAGE_JSON.build;
 
+const app:express.Application = express();
+const server:http.Server = http.createServer(<any>app);
 
-// Set up Middleware
-app.use(bodyParser.json());
-app.use(express.static(__dirname + "/../public"));
-
-// Set up server listening on port
-app.listen(8000);
-server.listen(PORT_NUMBER, function() {
-	console.log("Server started: listening on port " + PORT_NUMBER);
+//TODO: create a module for logging framework/utility/log.ts
+//	Set up custom colors.js theme for logging
+tc.setTheme({
+	aos: 'bold',
+	error: 'red',
+	title: 'magenta',
+	indent: 'grey'
 });
 
 
+/* ---------------------
+|	Set up middleware
+| --------------------- */
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/../public"));
 
 
-//  Define entry point
+/* -----------------------
+|	Bind server to port
+| ----------------------- */
+
+const io = SocketIO.listen(server);
+server.listen(PORT_NUMBER, function() {
+	console.log(tc.aos("[AOS]") + " Server bound to port: " + PORT_NUMBER);
+});
+
+
+/* -----------------------
+|	Define entry point
+| ----------------------- */
 
 function main():void {
-    
-    //  Create an instance of Framework/Program.
-            //  Init & Run Program
-    console.log("Hello serverside");
-    
+	
+	//TODO:
+		//  Create an instance of Framework/Program.
+			//	Init & Run Program
+	let startupMessage:string = (
+		tc.aos("\n\n[AOS] ") +
+		tc.title("Initialising AOS serverside framework...\n") +
+		tc.indent(`\t- AOS version:\t${AOS_VERSION}\n`) +
+		tc.indent(`\t- AOS build:\t${AOS_BUILD}\n`)
+	);
+	
+	console.log(startupMessage);
+	
 }
 
 main();
