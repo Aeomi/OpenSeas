992fe24ff135853d1f1c7b121b54592fd70d5435
Aeomi <aeomei@gmail.com>
2016-06-29 14:05:59 -0400

    gulpfile rework, using pump over pipe, correctly using node sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp        = require("gulp");
+var inject      = require("gulp-inject-string");
+// var uglify      = require("gulp-uglify");
+var pump        = require('pump');
 var typescript  = require("gulp-typescript");
 var sourceMap   = require("gulp-sourcemaps");
 
 var tsProj = typescript.createProject("tsconfig.json");
+var nodeSourcemapLoad = "require('source-map-support').install({environment: 'node'});"
 
-gulp.task("typescript", function() {
-    return gulp.src(["src/**/*.ts", "typings/tsd.d.ts"])
-        .pipe(sourceMap.init())
-        .pipe(typescript(tsProj))
-        .pipe(sourceMap.write("../sourcemaps"))
-        .pipe(gulp.dest("dist"));
+gulp.task("typescript", function(callback) {
+    pump([
+        gulp.src(["src/**/*.ts", "typings/tsd.d.ts"]),
+        sourceMap.init(),
+        typescript(tsProj),
+        inject.after('"use strict";', nodeSourcemapLoad),
+        sourceMap.write("../sourcemaps"),
+        gulp.dest("dist")
+    ],
+    callback)
 });
 
 gulp.task("watch", function() {
